23d0d2f0a6c5527537744027b232ac0dbd534ef3
Dhruv610ag <dhruv610agg@gmail.com>
2025-07-28 21:19:26 +0530

    newques

---
+++ Python/Two_sum.py
@@ -0,0 +1,13 @@
+#time-complexity-O(n)
+#space-complexity-O(n)
+from typing import List
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashmap = {}
+        for i, num in enumerate(nums):
+            complement = target - num
+            if complement in hashmap:
+                return [hashmap[complement], i]
+            hashmap[num] = i
+        return []
