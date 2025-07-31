043d06d6ad75325a1978a7192ec97f58e6f49fa4
Dhruv610ag <dhruv610agg@gmail.com>
2025-07-31 23:08:04 +0530

    newques

--- Python/Arrays/SecondLargest.py
+++ Python/Arrays/SecondLargest.py
@@ -1,1 +1,18 @@
-#
+# Time-Complexity-O(n)
+# Space Complexity-O(1)
+class Solution:
+    def getSecondLargest(self, arr):
+        n=len(arr)
+        if n<2:
+            return -1
+        first=second=-234567
+        for num in arr:
+            if num>first:
+                second=first
+                first=num
+            elif num>second and num!=first:
+                second=num
+        if second==-234567:
+            return -1
+        else:
+            return second
