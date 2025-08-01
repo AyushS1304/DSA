b88084e2488f2dec2068b8299677fcbe5f76d9f6
Dhruv610ag <dhruv610agg@gmail.com>
2025-08-01 12:01:09 +0530

    changes

--- Python/Arrays/isPerfectSquare.py
+++ Python/Arrays/isPerfectSquare.py
@@ -1,9 +1,27 @@
 # Time -Complexity-O(n)
 # Space Complexity-O(1)
 
-import math
+# import math
 
+# class Solution:
+#     def isPerfectSquare(self, num: int) -> bool:
+#         root = math.isqrt(num)
+#         return root * root == num
+    
+# Time-Complexity-O(nlogn)
+# Space Complexity-O(1)
 class Solution:
     def isPerfectSquare(self, num: int) -> bool:
-        root = math.isqrt(num)
-        return root * root == num
+        if num<0:
+            return False
+        l,h=0,num
+        while(l<=h):
+            mid=(l+h)//2
+            s=mid*mid
+            if s==num:
+                return True
+            elif s<num:
+                l=mid+1
+            else:
+                h=mid-1
+        return False
