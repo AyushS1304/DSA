78293c7dc8572fff97fef5df29aab01601e37890
AyushS1304 <ayushsharma130408@gmail.com>
2025-08-05 01:03:33 +0530

    New Question(Daily)

---
+++ DailyLeetcode/4Aug/FruitIntoBaskets.java
@@ -0,0 +1,28 @@
+// LeetCode Problem: 904. Fruit Into Baskets
+// Time Complexity: O(n) 
+// Space Complexity: O(1) (using a HashMap)
+
+import java.util.HashMap;
+
+
+class Solution {
+    public int totalFruit(int[] fruits) {
+        int start = 0,end = 0;
+        int n = fruits.length,maxLen = 0;
+        Map<Integer,Integer> map = new HashMap<>();
+        while(end<n)
+        {
+            map.put(fruits[end],map.getOrDefault(fruits[end],0)+1);
+            while(map.size()>=3)
+            {
+                map.put(fruits[start],map.get(fruits[start])-1);
+                if(map.get(fruits[start]) == 0) map.remove(fruits[start]);
+                start++;
+            }
+            int currLen = end-start+1;
+            maxLen = Math.max(maxLen,currLen);
+            end++;
+        }
+        return maxLen;
+    }
+}
