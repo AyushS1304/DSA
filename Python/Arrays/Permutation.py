0f6b0698ffc364c253e33bd4315d249931fd5fec
Dhruv610ag <dhruv610agg@gmail.com>
2025-08-09 11:30:54 +0530

    newques

---
+++ Python/Arrays/Permutation.py
@@ -0,0 +1,16 @@
+# Leetcode-46
+# Time-Complexity-O(n)
+# Space-Complexity-O(n)
+
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums)==0:
+            return [[]]
+        permus=self.permute(nums[1:])
+        res=[]
+        for p in permus:
+            for i in range(len(p)+1):
+                p_copy=p.copy()
+                p_copy.insert(i,nums[0])
+                res.append(p_copy)
+        return res
