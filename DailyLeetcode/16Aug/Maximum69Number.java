f4e69f487c40b18cf2ddf5273e75e98d742678b8
AyushS1304 <ayushsharma130408@gmail.com>
2025-08-16 23:50:49 +0530

    New Question(Daily)

---
+++ DailyLeetcode/16Aug/Maximum69Number.java
@@ -0,0 +1,15 @@
+// Solved using greedy approach
+//Time Complexity: O(d)
+//Space Complexity: O(d)
+class Solution {
+    public int maximum69Number(int num) {
+  char[] digits = String.valueOf(num).toCharArray();  
+        for (int i = 0; i < digits.length; i++) {
+            if (digits[i] == '6') {
+                digits[i] = '9';
+                break; 
+            }
+        }
+        return Integer.parseInt(new String(digits));
+    }
+}
