ee4f5ac4bb1ac9e5d132e2dc98ccbbde73586496
Dhruv610ag <dhruv610agg@gmail.com>
2025-08-19 22:18:04 +0530

    newsques

---
+++ DailyLeetcode/19Aug/NumberofZeroFilledSubarrays.py
@@ -0,0 +1,13 @@
+# time-complexity-O(n)
+# space complexity-O(1)
+class Solution:
+    def zeroFilledSubarray(self, nums: List[int]) -> int:
+        count=0
+        result=0
+        for num in nums:
+            if num==0:
+                count+=1
+                result=result+count
+            else:
+                count=0
+        return result
